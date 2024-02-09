branchseer/oxc

commit a12171a71d4090fb10c23ffdd357e4864f348c58

perf(parser): lex identifiers as bytes not chars (#2352)
This PR re-implements lexing identifiers with a fast path for the most common case - identifiers which are pure ASCII characters, using the new `Source` / `SourcePosition` APIs.

Lexing identifiers is a hot path, and accounts for the majority of the time the Lexer spends. The performance bump from this change is (if I do say so myself!) quite decent.

I've spent a lot of time tuning the implementation, which gained a further 10-15% on the Lexer benchmarks compared to my first, simpler attempt. Some of the design decisions, if they look odd, are likely motivated by gains in performance.

### Techniques

This implementation uses a few different strategies for performance:

* Search byte-by-byte, not char-by-char.
* Process batches of 32 bytes at a time to reduce bounds checks.
* Mark uncommon paths `#[cold]`.

### Structure

The implementation is built in 3 layers:

1. ASCII characters only.
2. ASCII and Unicode characters.
3. `\` escape sequences (and all the above).

`identifier_name_handler` starts at the top layer, and is optimized for consuming ASCII as fast as possible. Each "layer" is considered more uncommon than the previous, and dropping down a layer is a de-opt.

I'm assuming that 95%+ of JavaScript code does not include either Unicode characters or escapes in identifiers, so the speed of the fast path is prioritised.

That said, once a Unicode character is encountered, the next layer does expect to find further Unicode characters, rather than de-opting over and over again. If an identifier *starts* with a Unicode character, it enters the code straight on the 2nd layer, so is not penalised by going through a `#[cold]` boundary. Lexing Unicode is never going to be as fast as ASCII, but still I felt it was important not to penalise it unnecessarily, so as not to be Anglo-centric.

### ASCII search macro

crates/oxc_parser/src/lexer/byte_handlers.rs
@@ -1,4 +1,4 @@
-use super::{AutoCow, Kind, Lexer, LexerContext};
+use super::{Kind, Lexer, LexerContext};
 use crate::diagnostics;
 
 #[allow(clippy::unnecessary_safety_comment)]
@@ -137,7 +137,10 @@ macro_rules! ascii_byte_handler {
 /// (`a`-`z`, `A`-`Z`, `$` or `_`).
 ///
 /// Macro calls `Lexer::identifier_name_handler` to get the text of the identifier,
-/// and slices off first character.
+/// minus its first character.
+///
+/// `Lexer::identifier_name_handler` is an unsafe function, but if byte being consumed is ASCII,
+/// its requirements are met.
 ///
 /// # SAFETY
 /// Only use this macro to define byte handlers for ASCII characters.
@@ -156,7 +159,8 @@ macro_rules! ascii_byte_handler {
 /// const L_G: ByteHandler = {
 ///   #[allow(non_snake_case)]
 ///   fn L_G(lexer: &mut Lexer) -> Kind {
-///     let id_without_first_char = &lexer.identifier_name_handler()[1..];
+///     // SAFETY: This macro is only used for ASCII characters
+///     let id_without_first_char = unsafe { lexer.identifier_name_handler() };
 ///     match id_without_first_char {
 ///       "et" => Kind::Get,
 ///       "lobal" => Kind::Global,
@@ -169,7 +173,8 @@ macro_rules! ascii_byte_handler {
 macro_rules! ascii_identifier_handler {
     ($id:ident($str:ident) $body:expr) => {
         byte_handler!($id(lexer) {
-            let $str = &lexer.identifier_name_handler()[1..];
+            // SAFETY: This macro is only used for ASCII characters
+            let $str = unsafe { lexer.identifier_name_handler() };
             $body
         });
     };
@@ -439,12 +444,7 @@ ascii_byte_handler!(BTO(lexer) {
 
 // \
 ascii_byte_handler!(ESC(lexer) {
-    let mut builder = AutoCow::new(lexer);
-    lexer.consume_char();
-    builder.force_allocation_without_current_ascii_char(lexer);
-    lexer.identifier_unicode_escape_sequence(&mut builder, true);
-    let text = lexer.identifier_name(builder);
-    Kind::match_keyword(text)
+    lexer.identifier_backslash_handler()
 });
 
 // ]

crates/oxc_parser/src/lexer/mod.rs
@@ -16,6 +16,7 @@ mod number;
 mod numeric;
 mod punctuation;
 mod regex;
+mod search;
 mod source;
 mod string;
 mod string_builder;
@@ -302,4 +303,10 @@ impl<'a> Lexer<'a> {
             }
         }
     }
+}
+
+/// Call a closure while hinting to compiler that this branch is rarely taken.
+#[cold]
+pub fn cold_branch<F: FnOnce() -> T, T>(f: F) -> T {
+    f()
 }

Cargo.toml
@@ -111,6 +111,7 @@ regex                     = { version = "1.10.3" }
 rustc-hash                = { version = "1.1.0", default-features = false, features = ["std"] }
 ryu-js                    = { version = "1.0.0" }
 ropey                     = { version = "1.6.1" }
+seq-macro                 = { version = "0.3.5" }
 serde                     = { version = "1.0.196" }
 serde_json                = { version = "1.0.113" }
 syn                       = { version = "=1.0.109" }

crates/oxc_parser/Cargo.toml
@@ -29,6 +29,7 @@ assert-unchecked = { workspace = true }
 bitflags         = { workspace = true }
 rustc-hash       = { workspace = true }
 num-bigint       = { workspace = true }
+seq-macro        = { workspace = true }
 
 [dev-dependencies]
 oxc_ast    = { workspace = true, features = ["serde"] }

crates/oxc_parser/src/lexer/identifier.rs
@@ -1,66 +1,300 @@
-use super::{AutoCow, Kind, Lexer, Span};
+use super::{
+    cold_branch,
+    search::{byte_search, safe_byte_match_table, SafeByteMatchTable},
+    Kind, Lexer, SourcePosition,
+};
 use crate::diagnostics;
 
-use oxc_syntax::identifier::{is_identifier_part, is_identifier_start};
+use std::cmp::max;
+
+use oxc_allocator::String;
+use oxc_span::Span;
+use oxc_syntax::identifier::{
+    is_identifier_part, is_identifier_part_unicode, is_identifier_start_unicode,
+};
+
+const MIN_ESCAPED_STR_LEN: usize = 16;
+
+static ASCII_ID_START_TABLE: SafeByteMatchTable =
+    safe_byte_match_table!(|b| b.is_ascii_alphabetic() || b == b'_' || b == b'$');
+
+static NOT_ASCII_ID_CONTINUE_TABLE: SafeByteMatchTable =
+    safe_byte_match_table!(|b| !(b.is_ascii_alphanumeric() || b == b'_' || b == b'$'));
+
+#[inline]
+fn is_identifier_start_ascii_byte(byte: u8) -> bool {
+    ASCII_ID_START_TABLE.matches(byte)
+}
 
 impl<'a> Lexer<'a> {
-    /// Section 12.7.1 Identifier Names
-    pub(super) fn identifier_name_handler(&mut self) -> &'a str {
-        let builder = AutoCow::new(self);
-        self.consume_char();
-        self.identifier_name(builder)
-    }
+    /// Handle identifier with ASCII start character.
+    /// Returns text of the identifier, minus its first char.
+    ///
+    /// Start character should not be consumed from `self.source` prior to calling this.
+    ///
+    /// This function is the "fast path" for the most common identifiers in JS code -
+    /// purely consisting of ASCII characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `$`.
+    /// JS syntax also allows Unicode identifiers and escapes (e.g. `\u{FF}`) in identifiers,
+    /// but they are very rare in practice. So this fast path will handle 99% of JS code.
+    ///
+    /// When Unicode or an escape is encountered, this function de-opts to paths which handle those
+    /// cases, but those paths are marked `#[cold]` to keep the ASCII fast path as fast as possible.
+    ///
+    /// The fast path uses pointers and unsafe code to minimize bounds checks etc.
+    /// The functions it delegates to for uncommon cases are both more complex, and less critical,
+    /// so they stick to safe code only.
+    ///
+    /// # SAFETY
+    /// * `self.source` must not be exhausted (at least 1 char remaining).
+    /// * Next char must be ASCII.
+    #[allow(clippy::missing_safety_doc)] // Clippy is wrong!
+    pub(super) unsafe fn identifier_name_handler(&mut self) -> &'a str {
+        // Advance past 1st byte.
+        // SAFETY: Caller guarantees not at EOF, and next byte is ASCII.
+        let after_first = self.source.position().add(1);
 
-    pub(super) fn identifier_name(&mut self, builder: AutoCow<'a>) -> &'a str {
-        self.identifier_tail(builder)
-    }
-
-    pub(super) fn private_identifier(&mut self) -> Kind {
-        let mut builder = AutoCow::new(self);
-        let start = self.offset();
-        match self.next_char() {
-            Some(c) if is_identifier_start(c) => {
-                builder.push_matching(c);
-            }
-            Some('\\') => {
-                builder.force_allocation_without_current_ascii_char(self);
-                self.identifier_unicode_escape_sequence(&mut builder, true);
-            }
-            Some(c) => {
-                #[allow(clippy::cast_possible_truncation)]
-                self.error(diagnostics::InvalidCharacter(
-                    c,
-                    Span::new(start, start + c.len_utf8() as u32),
-                ));
-                return Kind::Undetermined;
-            }
-            None => {
-                self.error(diagnostics::UnexpectedEnd(Span::new(start, start)));
-                return Kind::Undetermined;
-            }
-        }
-        self.identifier_tail(builder);
-        Kind::PrivateIdentifier
-    }
-
-    fn identifier_tail(&mut self, mut builder: AutoCow<'a>) -> &'a str {
-        // ident tail
-        while let Some(c) = self.peek() {
-            if !is_identifier_part(c) {
-                if c == '\\' {
-                    self.consume_char();
-                    builder.force_allocation_without_current_ascii_char(self);
-                    self.identifier_unicode_escape_sequence(&mut builder, false);
-                    continue;
+        // Consume bytes which are part of identifier
+        byte_search! {
+            lexer: self,
+            table: NOT_ASCII_ID_CONTINUE_TABLE,
+            start: after_first,
+            handle_match: |next_byte| {
+                // Found a matching byte.
+                // Either end of identifier found, or a Unicode char, or `\` escape.
+                // Handle uncommon cases in cold branches to keep the common ASCII path
+                // as fast as possible.
+                if !next_byte.is_ascii() {
+                    return cold_branch(|| {
+                        // SAFETY: `after_first` is position after consuming 1 byte, so subtracting 1
+                        // makes `start_pos` `source`'s position as it was at start of this function
+                        let start_pos = unsafe { after_first.sub(1) };
+                        &self.identifier_tail_unicode(start_pos)[1..]
+                    });
                 }
+                if next_byte == b'\\' {
+                    return cold_branch(|| {
+                        // SAFETY: `after_first` is position after consuming 1 byte, so subtracting 1
+                        // makes `start_pos` `source`'s position as it was at start of this function
+                        let start_pos = unsafe { after_first.sub(1) };
+                        &self.identifier_backslash(start_pos, false)[1..]
+                    });
+                }
+
+                // Return identifier minus its first char.
+                // SAFETY: `after_first` was position of `lexer.source` at start of this search.
+                // Searching only proceeds in forwards direction, so `lexer.source.position()`
+                // cannot be before `after_first`.
+                unsafe { self.source.str_from_pos_to_current_unchecked(after_first) }
+            },
+            handle_eof: || {
+                // Return identifier minus its first char.
+                // SAFETY: `lexer.source` is positioned at EOF, so there is no valid value
+                // of `after_first` which could be after current position.
+                unsafe { self.source.str_from_pos_to_current_unchecked(after_first) }
+            },
+        };
+    }
+
+    /// Handle rest of identifier after first byte of a multi-byte Unicode char found.
+    /// Any number of characters can have already been consumed from `self.source` prior to it.
+    /// `self.source` should be positioned at start of Unicode character.
+    fn identifier_tail_unicode(&mut self, start_pos: SourcePosition) -> &'a str {
+        let c = self.peek().unwrap();
+        if is_identifier_part_unicode(c) {
+            self.consume_char();
+            self.identifier_tail_after_unicode(start_pos)
+        } else {
+            // Reached end of identifier. Return identifier.
+            self.source.str_from_pos_to_current(start_pos)
+        }
+    }
+
+    /// Handle identifier after first char (which was Unicode) is dealt with.
+    ///
+    /// First char should have been consumed from `self.source` prior to calling this.
+    /// `start_pos` should be position of the start of the identifier (before first char was consumed).
+    pub(super) fn identifier_tail_after_unicode(&mut self, start_pos: SourcePosition) -> &'a str {
+        // Identifier contains a Unicode chars, so probably contains more.
+        // So just iterate over chars now, instead of bytes.
+        while let Some(c) = self.peek() {
+            if is_identifier_part(c) {
+                self.consume_char();
+            } else if c == '\\' {
+                // This branch marked cold as escapes are uncommon
+                return cold_branch(|| self.identifier_backslash(start_pos, false));
+            } else {
                 break;
             }
-            self.consume_char();
-            builder.push_matching(c);
         }
-        let has_escape = builder.has_escape();
-        let text = builder.finish(self);
-        self.save_string(has_escape, text);
-        text
+
+        // Return identifier
+        self.source.str_from_pos_to_current(start_pos)
+    }
+
+    /// Handle identifier starting with `\` escape.
+    pub fn identifier_backslash_handler(&mut self) -> Kind {
+        // Create arena string to hold unescaped identifier.
+        // We don't know how long identifier will end up being, so guess.
+        let str = String::with_capacity_in(MIN_ESCAPED_STR_LEN, self.allocator);
+
+        // Process escape and get rest of identifier
+        let id = self.identifier_on_backslash(str, true);
+        Kind::match_keyword(id)
+    }
+
+    /// Consume rest of identifier after a `\` escape is found.
+    ///
+    /// The `\` must not have be consumed from `lexer.source`.
+    /// `start_pos` must be position of start of identifier.
+    fn identifier_backslash(&mut self, start_pos: SourcePosition, is_start: bool) -> &'a str {
+        // Create arena string to hold unescaped identifier.
+        // We don't know how long identifier will end up being. Take a guess that total length
+        // will be double what we've seen so far, or `MIN_ESCAPED_STR_LEN` minimum.
+        let so_far = self.source.str_from_pos_to_current(start_pos);
+        let capacity = max(so_far.len() * 2, MIN_ESCAPED_STR_LEN);
+        let mut str = String::with_capacity_in(capacity, self.allocator);
+
+        // Push identifier up this point into `str`
+        // `bumpalo::collections::string::String::push_str` is currently expensive due to
+        // inefficiency in bumpalo's implementation. But best we have right now.
+        str.push_str(so_far);
+
+        // Process escape and get rest of identifier
+        self.identifier_on_backslash(str, is_start)
+    }
+
+    /// Process rest of identifier after a `\` found.
+    ///
+    /// `self.source` should be positioned *on* the `\` (i.e. `\` has not been consumed yet).
+    /// `str` should contain the identifier up to before the escape.
+    /// `is_start` should be `true` if this is first char in the identifier, `false` otherwise.
+    fn identifier_on_backslash(&mut self, mut str: String<'a>, mut is_start: bool) -> &'a str {
+        'outer: loop {
+            // Consume `\`
+            self.consume_char();
+
+            // Consume escape sequence and add char to `str`
+            self.identifier_unicode_escape_sequence(&mut str, is_start);
+            is_start = false;
+
+            // Consume chars until reach end of identifier or another escape
+            let chunk_start = self.source.position();
+            loop {
+                let maybe_char = self.peek();
+                if maybe_char.is_some_and(is_identifier_part) {
+                    self.consume_char();
+                    continue;
+                }
+
+                // End of identifier, EOF, or another `\` escape.
+                // Push chunk since last escape to `str`.
+                let chunk = self.source.str_from_pos_to_current(chunk_start);
+                str.push_str(chunk);
+
+                if maybe_char != Some('\\') {
+                    // End of identifier or EOF
+                    break 'outer;
+                }
+
+                // Found another escape. Go back to start of outer loop.
+                break;
+            }
+        }
+
+        // Convert `str` to arena slice and save to `escaped_strings`
+        let id = str.into_bump_str();
+        self.save_string(true, id);
+        id
+    }
+
+    /// Entry point for a private identifier. i.e. after `#`.
+    /// `#` must be consumed before calling this.
+    ///
+    /// Like `identifier_name_handler`, this contains a fast path for identifiers which are pure ASCII.
+    /// Unicode characters and escapes are handled on paths marked `#[cold]` to keep the common ASCII
+    /// fast path as fast as possible.
+    pub fn private_identifier(&mut self) -> Kind {
+        // Handle EOF directly after `#`
+        let start_pos = self.source.position();
+        if start_pos.addr() == self.source.end_addr() {
+            return cold_branch(|| {
+                let start = self.offset();
+                self.error(diagnostics::UnexpectedEnd(Span::new(start, start)));
+                Kind::Undetermined
+            });
+        }
+
+        // Handle if not an ASCII identifier byte.
+        // SAFETY: Not at EOF, so safe to read a byte.
+        let b = unsafe { start_pos.read() };
+        if !is_identifier_start_ascii_byte(b) {
+            return self.private_identifier_not_ascii_id();
+        }
+
+        // SAFETY: Not at EOF, so can advance 1 byte without going out of bounds
+        let after_first = unsafe { start_pos.add(1) };
+
+        // Consume bytes which are part of identifier
+        byte_search! {
+            lexer: self,
+            table: NOT_ASCII_ID_CONTINUE_TABLE,
+            start: after_first,
+            handle_match: |next_byte| {
+                // Found a matching byte.
+                // Either end of identifier found, or a Unicode char, or `\` escape.
+                // Handle uncommon cases in cold branches to keep the common ASCII path
+                // as fast as possible.
+                if !next_byte.is_ascii() {
+                    return cold_branch(|| {
+                        // SAFETY: `after_first` is position after consuming 1 byte, so subtracting 1
+                        // makes `start_pos` `source`'s position as it was at start of this function
+                        let start_pos = unsafe { after_first.sub(1) };
+                        self.identifier_tail_unicode(start_pos);
+                        Kind::PrivateIdentifier
+                    });
+                }
+                if next_byte == b'\\' {
+                    return cold_branch(|| {
+                        // SAFETY: `after_first` is position after consuming 1 byte, so subtracting 1
+                        // makes `start_pos` `source`'s position as it was at start of this function
+                        let start_pos = unsafe { after_first.sub(1) };
+                        self.identifier_backslash(start_pos, false);
+                        Kind::PrivateIdentifier
+                    });
+                }
+
+                Kind::PrivateIdentifier
+            },
+            handle_eof: || {
+                Kind::PrivateIdentifier
+            },
+        };
+    }
+
+    /// Handle private identifier whose first byte is not an ASCII identifier start byte.
+    #[cold]
+    fn private_identifier_not_ascii_id(&mut self) -> Kind {
+        let b = self.source.peek_byte().unwrap();
+        if !b.is_ascii() {
+            let c = self.peek().unwrap();
+            if is_identifier_start_unicode(c) {
+                let start_pos = self.source.position();
+                self.consume_char();
+                self.identifier_tail_after_unicode(start_pos);
+                return Kind::PrivateIdentifier;
+            }
+        } else if b == b'\\' {
+            // Assume Unicode characters are more common than `\` escapes, so this branch as cold
+            return cold_branch(|| {
+                self.identifier_backslash_handler();
+                Kind::PrivateIdentifier
+            });
+        }
+
+        // No identifier found
+        let start = self.offset();
+        let c = self.consume_char();
+        self.error(diagnostics::InvalidCharacter(c, Span::new(start, self.offset())));
+        Kind::Undetermined
     }
 }

commit 80c7ea91e614c2cdd36f1abd06ee834994b70e29

perf(parser): eat whitespace after line break (#2353)
Uses the `byte_search!` macro introduced in #2352 to consume whitespace after a line break.

crates/oxc_parser/src/lexer/byte_handlers.rs
@@ -203,8 +203,7 @@ ascii_byte_handler!(ISP(lexer) {
 // '\r' '\n'
 ascii_byte_handler!(LIN(lexer) {
     lexer.consume_char();
-    lexer.token.is_on_new_line = true;
-    Kind::Skip
+    lexer.line_break_handler()
 });
 
 // !

crates/oxc_parser/src/lexer/mod.rs
@@ -25,6 +25,7 @@ mod token;
 mod trivia_builder;
 mod typescript;
 mod unicode;
+mod whitespace;
 
 use rustc_hash::FxHashMap;
 use std::collections::VecDeque;

crates/oxc_parser/src/lexer/whitespace.rs
@@ -1,0 +1,28 @@
+use super::{
+    search::{byte_search, safe_byte_match_table, SafeByteMatchTable},
+    Kind, Lexer,
+};
+
+static NOT_REGULAR_WHITESPACE_OR_LINE_BREAK_TABLE: SafeByteMatchTable =
+    safe_byte_match_table!(|b| !matches!(b, b' ' | b'\t' | b'\r' | b'\n'));
+
+impl<'a> Lexer<'a> {
+    pub(super) fn line_break_handler(&mut self) -> Kind {
+        self.token.is_on_new_line = true;
+
+        // Indentation is common after a line break.
+        // Consume it, along with any further line breaks.
+        // Irregular line breaks and whitespace are not consumed.
+        // They're uncommon, so leave them for the next call to `handle_byte` to take care of.
+        byte_search! {
+            lexer: self,
+            table: NOT_REGULAR_WHITESPACE_OR_LINE_BREAK_TABLE,
+            handle_match: |_next_byte, _start| {
+                Kind::Skip
+            },
+            handle_eof: |_start| {
+                Kind::Skip
+            },
+        };
+    }
+}